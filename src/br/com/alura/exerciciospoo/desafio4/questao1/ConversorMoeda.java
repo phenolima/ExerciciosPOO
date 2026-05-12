0efc26f3ce180c5aebe6b3eb1b325ef10de6ece9
phenolima <pedrooliveira0997@gmail.com>
2026-05-12 00:30:28 -0300

    feat: adicionando desafio 4 questao 1

---
+++ src/br/com/alura/exerciciospoo/desafio4/questao1/ConversorMoeda.java
@@ -0,0 +1,18 @@
+package br.com.alura.exerciciospoo.desafio4.questao1;
+
+public class ConversorMoeda implements ConversaoFinanceira{
+     private double taxaDeCambio;
+
+    public double getTaxaDeCambio() {
+        return taxaDeCambio;
+    }
+
+    public void setTaxaDeCambio(double taxaDeCambio) {
+        this.taxaDeCambio = taxaDeCambio;
+    }
+
+    @Override
+    public double converteDolarParaReal(double valor) {
+        return taxaDeCambio * valor;
+    }
+}
